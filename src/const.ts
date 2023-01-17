5668aec880ff6c0e0fe60de888106805a5c7b281
Andrew <8911102@gmail.com>
2023-01-17 13:13:53 +0500

    constants for allowed banknotes were created

---
+++ src/const.ts
@@ -0,0 +1,11 @@
+const allowedBanknotes = [50, 100, 500, 1000];
+const allowedChangeBanknotes = [1, 5, 10, 50, 100, 500];
+
+enum goodTypes {
+  mineralWater = 'Mineral Water',
+  soda = 'Chips',
+  juices = 'Juices',
+  chewingGum = 'Chewing Gum',
+  chocolate = 'Chocolate',
+  cookies = 'Cookies'
+};
