54cce04cbe4da030a6205d91503972593db76819
Andrew <8911102@gmail.com>
2023-01-17 15:46:41 +0500

    back-up was added for db

--- src/const.ts
+++ src/const.ts
@@ -1,7 +1,7 @@
-export const allowedBanknotes = [50, 100, 500, 1000];
-export const allowedChangeBanknotes = [1, 5, 10, 50, 100, 500];
+export const acceptedBanknotes = [50, 100, 500, 1000];
+export const changeBanknotes = [1, 5, 10, 50, 100, 500];
 
-export enum goodsTypes {
+export enum GoodsTypes {
   mineralWater = 'Mineral Water',
   soda = 'Soda',
   juices = 'Juices',
