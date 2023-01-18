277a8f85e1dbe5eecabbe007468c03ee2ea51364
Andrew <8911102@gmail.com>
2023-01-18 20:51:45 +0500

    basic styles were added for descktop version

--- src/components/purchased/purchased.jsx
+++ src/components/purchased/purchased.jsx
@@ -5,18 +5,16 @@ import ChangeList from '../change-list/change-list';
 export function Purchased(props) {
   return (
     <div className="wrapper">
-    <div className='purchased'>
-      <div>
+      <div className='purchased'>
         <h4>Purchased goods</h4>
-        <ul>
+        <ul className='purchased__list'>
           List of bought goods
         </ul>
       </div>
-      <div>
+      <div className='change'>
         <button className='button'>Get change</button>
         <h4>Your change</h4>
         <ChangeList />
-      </div>
       </div>
     </div>
   );
