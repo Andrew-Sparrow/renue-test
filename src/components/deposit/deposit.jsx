31b162a6e1caba437a831f1c8e2dd128d05f49d3
Andrew <8911102@gmail.com>
2023-01-18 16:21:18 +0500

    deposit block was stylized

--- src/components/deposit/deposit.jsx
+++ src/components/deposit/deposit.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 export function Deposit(props) {
   return (
     <div className='deposit'>
-      <p className='deposit__info'>Amount of money deposited<span>{}</span></p>
-      <p className='deposit__info'>The amount of money left<span>{}</span></p>
-      <p className='deposit__info deposit__info--warning'>Warning<span>{}</span></p>
+      <p className='deposit__info'>Amount of money deposited</p>
+      <p className='deposit__value'>{}</p>
+      <p className='deposit__info'>The amount of money left</p>
+      <p className='deposit__value'>{}</p>
+      <p className='deposit__info deposit__info--warning'>Warning</p>
+      <p className='deposit__value'>{}</p>
       <ul className='goods'>
         List of goods
       </ul>
