b6a1fa7448cd70da816fa47f73954cfad80a1ef0
Andrew <8911102@gmail.com>
2023-01-18 22:47:49 +0500

    verification for input field for amout baknotes was added

--- src/components/banknote/banknote.jsx
+++ src/components/banknote/banknote.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export function Banknote({denomination}) {
+const invalidChars = [
+  "-",
+  "+",
+  "e",
+];
+
+export function Banknote({ denomination }) {
+  const [value, setValue] = useState(0);
+  const [errorText, setErrorText] = useState('');
+
+  function handleChangeInput(evt) {
+    const value = evt.target.value;
+
+    if (isNaN(value)) {
+      setErrorText('Enter only number');
+      setValue(0);
+    } else {
+      setValue(+evt.target.value);
+      setErrorText('');
+    }
+  }
+
   return (
     <div className='banknote'>
       <p className="denomination">{denomination}</p><span> X </span>
-      <input className='banknote__input' type="number" />
+      <div>
+        <input
+          className='banknote__input'
+          type="text"
+          onChange={handleChangeInput}
+          value={value}
+        ></input>
+        <p className="banknote__error">{errorText}</p>
+      </div>
     </div>
   );
 }
