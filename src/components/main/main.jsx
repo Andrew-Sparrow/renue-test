8bbf9c5676705b756a8945c43b9b6f6d0db74cdc
Andrew <8911102@gmail.com>
2023-01-21 14:04:38 +0500

    add link to github

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Money } from '../money/money.tsx';
 import { Deposit } from '../deposit/deposit.jsx';
 import { Purchased } from '../purchased/purchased.jsx';
@@ -9,6 +9,13 @@ export function Main() {
   return (
     <main className='main'>
       <h1 className='main__title'>Vending Machine</h1>
+      <a
+        className="main__link"
+        target="_blank"
+        href='https://github.com/Andrew-Sparrow/renue-test'
+        alt='Link to GitHub'
+        rel="noopener noreferrer"
+      >Project on GitHub</a>
       <div className='wrapper'>
         <Deposit />
         <Money />
